9aeab1222d64f002f9656ef682a428813524b609
gabrielcellone <124008317+gabrielcellone@users.noreply.github.com>
2023-05-15 13:27:19 -0300

    Add files via upload

---
+++ ENTREGA FRONT END/funciones_fe.js
@@ -0,0 +1,12 @@
+document.getElementById('boton').addEventListener('click', function(){
+    document.getElementById("párrafo").innerHTML = "Estoy revisando su mensaje...";
+ });
+
+
+ function mostrarTexto() {
+   document.getElementById('hideText').style.display = 'block';
+  
+ }
+   function ocultarTexto() {
+      document.getElementById('hideText').style.display = 'none';
+       }
